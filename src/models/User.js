6916ac33c131ffc69fa6eb1f260827f400d49472
Amit Upadhyay <devcoder.blog@outlook.com>
2022-07-24 00:09:41 +0530

    Password Hashing and salt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -10,5 +11,38 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+//pre save hook, defined function will run first
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  //10 how complex salt can we generatd
+  bcrypt.genSalt(10, (error, salt) => {
+    if (error) {
+      return next(error);
+    }
+
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) return reject(err);
+      if (!isMatch) return reject(false);
+      resolve(true);
+    });
+  });
+};
 
 mongoose.model('User', userSchema);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -25,4 +25,25 @@ router.post('/signup', async (req, res) => {
   res.send('you have made some request');
 });
 
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+
+  const user = await User.findOne({ email: email });
+  if (!user) return res.status(404).send({ error: 'Email no found' });
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'MY_SECREAT_KEY');
+    console.log(token);
+    res.send({ token });
+  } catch (err) {
+    console.error('error', err);
+    return res.status(422).send({ error: 'Invalid password or email' });
+  }
+});
+
 module.exports = router;
